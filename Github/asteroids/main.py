37983ca8b440ed00b682858b72696c356f956ead
mwildcap <m.wildschut@wildcap.nl>
2025-08-18 16:41:18 +0200

    third commit

--- Github/asteroids/main.py
+++ Github/asteroids/main.py
@@ -8,12 +8,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time()
 
 
 if __name__ == "__main__":
