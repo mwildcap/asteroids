d3c0d657f4513c1ea61535f91c423a50bb9c9f53
mwildcap <m.wildschut@wildcap.nl>
2025-08-18 20:57:17 +0200

    fifth

--- Github/asteroids/main.py
+++ Github/asteroids/main.py
@@ -21,12 +21,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill(("black"))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.get_time()
-        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
